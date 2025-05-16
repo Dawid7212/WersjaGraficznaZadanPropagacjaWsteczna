7a9171fc84d993c37d40ad4cedc44cc3246c8600
Dawid7212 <dawidMoeck002@gmail.com>
2025-05-16 16:10:36 +0200

    Dodaj pliki projektów.

---
+++ Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace WersjagraficznaTychZadan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnTestuj = new System.Windows.Forms.Button();
+            this.labelWagiPoczatkowe = new System.Windows.Forms.Label();
+            this.labelWagiKoncowe = new System.Windows.Forms.Label();
+            this.WszystkieWyniki = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.WYniki12 = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(194, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Wybierz zadanie i zatwierdz przyciskiem";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "XOR",
+            "XOR_NOR",
+            "Sumatorek"});
+            this.comboBox1.Location = new System.Drawing.Point(44, 74);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(44, 101);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Zatwierdz";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnTestuj
+            // 
+            this.btnTestuj.Location = new System.Drawing.Point(44, 130);
+            this.btnTestuj.Name = "btnTestuj";
+            this.btnTestuj.Size = new System.Drawing.Size(75, 23);
+            this.btnTestuj.TabIndex = 4;
+            this.btnTestuj.Text = "Testuj";
+            this.btnTestuj.UseVisualStyleBackColor = true;
+            this.btnTestuj.Click += new System.EventHandler(this.btnTestuj_Click);
+            // 
+            // labelWagiPoczatkowe
+            // 
+            this.labelWagiPoczatkowe.AutoSize = true;
+            this.labelWagiPoczatkowe.Location = new System.Drawing.Point(360, 172);
+            this.labelWagiPoczatkowe.Name = "labelWagiPoczatkowe";
+            this.labelWagiPoczatkowe.Size = new System.Drawing.Size(93, 13);
+            this.labelWagiPoczatkowe.TabIndex = 7;
+            this.labelWagiPoczatkowe.Text = "Wagi poczatkowe";
+            // 
+            // labelWagiKoncowe
+            // 
+            this.labelWagiKoncowe.AutoSize = true;
+            this.labelWagiKoncowe.Location = new System.Drawing.Point(360, 301);
+            this.labelWagiKoncowe.Name = "labelWagiKoncowe";
+            this.labelWagiKoncowe.Size = new System.Drawing.Size(79, 13);
+            this.labelWagiKoncowe.TabIndex = 8;
+            this.labelWagiKoncowe.Text = "Wagi koncowe";
+            // 
+            // WszystkieWyniki
+            // 
+            this.WszystkieWyniki.Location = new System.Drawing.Point(363, 26);
+            this.WszystkieWyniki.Name = "WszystkieWyniki";
+            this.WszystkieWyniki.Size = new System.Drawing.Size(286, 126);
+            this.WszystkieWyniki.TabIndex = 9;
+            this.WszystkieWyniki.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(360, 10);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Wyjscia po uczeniu:";
+            // 
+            // WYniki12
+            // 
+            this.WYniki12.Location = new System.Drawing.Point(44, 201);
+            this.WYniki12.Name = "WYniki12";
+            this.WYniki12.Size = new System.Drawing.Size(269, 142);
+            this.WYniki12.TabIndex = 11;
+            this.WYniki12.Text = "";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(41, 185);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(148, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Wyjscia sieci przed uczeniem:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(30, 428);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(460, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "*Każde kolejne kliknęcie przycisku \"Testuj\" powoduje wylosowanie wag i uczenie si" +
+    "eci na nowo";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.WYniki12);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.WszystkieWyniki);
+            this.Controls.Add(this.labelWagiKoncowe);
+            this.Controls.Add(this.labelWagiPoczatkowe);
+            this.Controls.Add(this.btnTestuj);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnTestuj;
+        private System.Windows.Forms.Label labelWagiPoczatkowe;
+        private System.Windows.Forms.Label labelWagiKoncowe;
+        private System.Windows.Forms.RichTextBox WszystkieWyniki;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox WYniki12;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,374 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar;
+
+namespace WersjagraficznaTychZadan
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var wybor = comboBox1.SelectedItem as string;
+            if (wybor == null) return;
+
+            labelWagiPoczatkowe.Text = "Wagi poczatkowe";
+            labelWagiKoncowe.Text = " Wagi koncowe";
+            btnTestuj.Visible = true;
+            WYniki12.Text = "";
+            WszystkieWyniki.Text = "";
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void btnTestuj_Click(object sender, EventArgs e)
+        {
+            string wybor = comboBox1.SelectedItem as string;
+            switch (wybor)
+            {
+                case "XOR":
+                    XOR();
+                    break;
+                case "XOR_NOR":
+                    XOR_NOR();
+                    break;
+                case "Sumatorek":
+                    Sumatorek();
+                    break;
+            }
+        }
+        private static Random rand = new Random();
+
+        private void XOR()
+        {
+            int[] struktura = { 2, 2, 1 };
+            double[][][] wagi = WylosujWagi(struktura);
+          //  double[][][] wagi = new double[2][][];
+           // wagi[0] = new double[2][];
+            //wagi[1] = new double[1][];
+           // wagi[0][0] = new double[] { 0.3, 0.1, 0.2 };
+           //// wagi[0][1] = new double[] { 0.6, 0.4, 0.5 };
+           // wagi[1][0] = new double[] { 0.9, 0.7, -0.8 };
+            double parametrUczenia = 0.3;
+            double[][] WejsciaSieci = new double[][]
+           {
+                    new double[] { 0, 0 },
+                    new double[] { 0, 1 },
+                    new double[] { 1, 0 },
+                    new double[] { 1, 1 }
+           };
+            double [][] WejsciaSieci2 = WejsciaSieci.Select(w => w.ToArray()).ToArray(); ;//kopia dla wynikow aby wyniki nie wyswitlaly sie pomieszane
+            double[] WyjsciaOczekiwane = { 0, 1, 1, 0 };
+            WszystkieWynikiPrzed(WejsciaSieci2, struktura, wagi);
+            GenerujWagiPoczatkowe(wagi);
+
+            for (int epoka = 0; epoka < 10000; epoka++)
+            {
+                if (epoka > 0)
+                {
+                    MieszajDane(WejsciaSieci, WyjsciaOczekiwane);
+                }
+                for (int i = 0; i < WejsciaSieci.Length; i++)
+                {
+                    wagi = PropagacjaWsteczna(WejsciaSieci[i], new[] { WyjsciaOczekiwane[i] }, wagi, struktura, parametrUczenia);
+                }
+            }
+
+            GenerujWagiKoncowe(wagi);
+            WypiszWszystkieWyniki(WejsciaSieci2, struktura, wagi);
+
+        }
+        private void XOR_NOR()
+        {
+            int[] struktura = { 2, 2, 2, 2 };
+            double[][][] wagi = WylosujWagi(struktura);
+            double parametrUczenia = 0.1;
+
+            double[][] WejsciaSieci = {
+                new double[] { 0, 0 },
+                new double[] { 0, 1 },
+                new double[] { 1, 0 },
+                new double[] { 1, 1 }
+            };
+            double[][] WejsciaSieci2 = WejsciaSieci.Select(w => w.ToArray()).ToArray(); ;
+            double[][] WyjsciaOczekiwane = {
+                new double[] { 0, 1 },
+                new double[] { 1, 0 },
+                new double[] { 1, 0 },
+                new double[] { 0, 0 }
+            };
+            WszystkieWynikiPrzed(WejsciaSieci2, struktura, wagi);
+            GenerujWagiPoczatkowe(wagi);
+            for (int epoka = 0; epoka < 40000; epoka++)
+            {
+                if (epoka > 0)
+                {
+                    MieszajDane(WejsciaSieci, WyjsciaOczekiwane);
+                }
+                for (int i = 0; i < WejsciaSieci.Length; i++)
+                {
+                    wagi = PropagacjaWsteczna(WejsciaSieci[i], WyjsciaOczekiwane[i], wagi, struktura, parametrUczenia);
+                }
+            }
+            GenerujWagiKoncowe(wagi);
+            WypiszWszystkieWyniki(WejsciaSieci2, struktura, wagi);
+        }
+        private void Sumatorek()
+        {
+            int[] struktura = { 3, 3, 2, 2 };
+            double[][][] wagi = WylosujWagi(struktura);
+            double parametrUczenia = 0.05;
+
+            double[][] WejsciaSieci = {
+                new double[] { 0, 0, 0 },
+                new double[] { 0, 0, 1 },
+                new double[] { 0, 1, 0 },
+                new double[] { 0, 1, 1 },
+                new double[] { 1, 0, 0 },
+                new double[] { 1, 0, 1 },
+                new double[] { 1, 1, 0 },
+                new double[] { 1, 1, 1 }
+             };
+            double[][] WejsciaSieci2 = WejsciaSieci.Select(w => w.ToArray()).ToArray(); ;
+            double[][] WyjsciaOczekiwane = {
+                new double[] { 0, 0 },
+                new double[] { 1, 0 },
+                new double[] { 1, 0 },
+                new double[] { 0, 1 },
+                new double[] { 1, 0 },
+                new double[] { 0, 1 },
+                new double[] { 0, 1 },
+                new double[] { 1, 1 }
+            };
+            WszystkieWynikiPrzed(WejsciaSieci2, struktura, wagi);
+            GenerujWagiPoczatkowe(wagi);
+            for (int epoka = 0; epoka < 80000; epoka++)
+            {
+                if (epoka > 0)
+                {
+                    MieszajDane(WejsciaSieci, WyjsciaOczekiwane);
+                }
+                for (int i = 0; i < WejsciaSieci.Length; i++)
+                {
+                    wagi = PropagacjaWsteczna(WejsciaSieci[i], WyjsciaOczekiwane[i], wagi, struktura, parametrUczenia);
+                }
+            }
+            GenerujWagiKoncowe(wagi);
+            WypiszWszystkieWyniki(WejsciaSieci2, struktura, wagi);
+        }
+
+
+
+        private static void MieszajDane(double[][] wejscia, double[] wyjscia)
+        {
+            for (int i = wejscia.Length - 1; i > 0; i--)
+            {
+                int j = rand.Next(i + 1);
+                (wejscia[i], wejscia[j]) = (wejscia[j], wejscia[i]);
+                (wyjscia[i], wyjscia[j]) = (wyjscia[j], wyjscia[i]);
+            }
+        }
+
+        private static void MieszajDane(double[][] wejscia, double[][] wyjscia)
+        {
+            for (int i = wejscia.Length - 1; i > 0; i--)
+            {
+                int j = rand.Next(i + 1);
+                (wejscia[i], wejscia[j]) = (wejscia[j], wejscia[i]);
+                (wyjscia[i], wyjscia[j]) = (wyjscia[j], wyjscia[i]);
+            }
+        }
+
+        public static double[][][] PropagacjaWsteczna(double[] wejscie, double[] OczekiwaneWyjscia, double[][][] wagi, int[] struktura, double parametrUczenia)
+        {
+
+            double[][] WynikiSieci = SiecNeuronowa(wejscie, wagi, struktura);
+            double B = 1;
+
+            double[][] BledyNeuronow = new double[struktura.Length - 1][]; //[NumerWarstwy][NumerNeuronu]
+
+
+            int ostatniaWarstwa = struktura.Length - 1;
+            BledyNeuronow[ostatniaWarstwa - 1] = new double[struktura[ostatniaWarstwa]];
+            for (int i = 0; i < struktura[ostatniaWarstwa]; i++)//dla neuronow warstwy otatniej
+            {
+                BledyNeuronow[ostatniaWarstwa - 1][i] = (OczekiwaneWyjscia[i] - WynikiSieci[ostatniaWarstwa][i]) * (B * WynikiSieci[ostatniaWarstwa][i] * (1 - WynikiSieci[ostatniaWarstwa][i]));
+
+            }
+
+            for (int nrWarstwy = struktura.Length - 2; nrWarstwy > 0; nrWarstwy--)//dla neuronow warstw ukrytych -> iteracja po warstwach
+            {
+                BledyNeuronow[nrWarstwy - 1] = new double[struktura[nrWarstwy]];
+                for (int i = 0; i < struktura[nrWarstwy]; i++)//iteracaja po liczbie neuronow w warstwie
+                {
+                    double SumaBledow = 0;
+                    for (int j = 0; j < struktura[nrWarstwy + 1]; j++)//iteracja po liczbie neuronów w warstwie kolejnej -> każdy neuron z warstwy obeznej ma połączenie ze wszystkimi neuronami warstwy nastepnej
+                    {
+                        SumaBledow += BledyNeuronow[nrWarstwy][j] * wagi[nrWarstwy][j][i + 1];//tylko dla połączeń miedzy neuronami, błąd neuronu kolejnego* waga łącząca te nurony
+                    }
+                    BledyNeuronow[nrWarstwy - 1][i] = SumaBledow * (B * WynikiSieci[nrWarstwy][i] * (1 - WynikiSieci[nrWarstwy][i]));
+                }
+
+            }
+
+            double[][][] WagiAfterUpdate = new double[wagi.Length][][];
+            for (int i = 0; i < wagi.Length; i++)
+            {
+                WagiAfterUpdate[i] = new double[wagi[i].Length][];
+                for (int j = 0; j < wagi[i].Length; j++)
+                {
+                    WagiAfterUpdate[i][j] = new double[wagi[i][j].Length];
+                    WagiAfterUpdate[i][j][0] = wagi[i][j][0] +   parametrUczenia*   BledyNeuronow[i][j];//waga ukryta jako pierwsza
+                    for (int k = 1; k < wagi[i][j].Length; k++)
+                    {
+                        WagiAfterUpdate[i][j][k] = wagi[i][j][k] + parametrUczenia * BledyNeuronow[i][j] * WynikiSieci[i][k - 1];
+                    }
+                }
+            }
+
+            return WagiAfterUpdate;
+        }
+
+        public static double[][] SiecNeuronowa(double[] wejscie, double[][][] Wagi, int[] struktura)
+        {
+            int lWarstw = struktura.Length;
+            double[][] wyjscie = new double[lWarstw][]; // to będzie zlepek wszystkich wyników neuronów podzielonych na warstwy
+
+            wyjscie[0] = new double[wejscie.Length];
+            for (int i = 0; i < wejscie.Length; i++)
+            {
+                wyjscie[0][i] = wejscie[i];
+            }
+
+
+            for (int i = 1; i < lWarstw; i++)
+            {
+                int LiczbaNeuronowWarstwy = struktura[i];
+
+                wyjscie[i] = new double[LiczbaNeuronowWarstwy];
+
+                for (int n = 0; n < LiczbaNeuronowWarstwy; n++)
+                {
+                    double[] wagiNeuronu = Wagi[i - 1][n];
+
+                    double z = Neuron(wagiNeuronu, wyjscie[i - 1]);
+                    wyjscie[i][n] = FAktywacji(z);
+                }
+            }
+            return wyjscie;
+        }
+        public static double FAktywacji(double WartNeurona, double B = 1.0)
+        {
+            return 1.0 / (1.0 + Math.Exp(-B * WartNeurona));
+        }
+
+        public static double Neuron(double[] Wagi, double[] wejscie)
+        {
+            double neuron = 0;
+            neuron += Wagi[0];
+            for (int j = 1; j <= wejscie.Length; j++)
+            {
+                neuron += Wagi[j] * wejscie[j - 1];
+            }
+            return neuron;
+        }
+
+        public static double[][][] WylosujWagi(int[] strukturaSieci)
+        {
+            //int liczbaWag = 0;
+            double[][][] wagi = new double[strukturaSieci.Length - 1][][];
+            for (int i = 0; i < strukturaSieci.Length - 1; i++)
+            {
+                wagi[i] = new double[strukturaSieci[i + 1]][];//+1 bo liczba wag zalezna od nastepnej warstwy
+                for (int j = 0; j < strukturaSieci[i + 1]; j++)
+                {
+                    wagi[i][j] = new double[strukturaSieci[i] + 1];
+                    for (int ktoraWaga = 0; ktoraWaga < strukturaSieci[i] + 1; ktoraWaga++)
+                    {
+                        wagi[i][j][ktoraWaga] = (rand.NextDouble() * 2) - 1;
+                    }
+                }
+            }
+
+
+            return wagi;
+        }
+        public void GenerujWagiPoczatkowe(double[][][] wagi)
+        {
+            StringBuilder sbPoczatkowe = new StringBuilder("Wagi początkowe:\n");
+            for (int i = 0; i < wagi.Length; i++)
+            {
+                for (int j = 0; j < wagi[i].Length; j++)
+                {
+                    sbPoczatkowe.Append($"Warstwa {i}, Neuron {j}: ");
+                    for (int k = 0; k < wagi[i][j].Length; k++)
+                    {
+                        sbPoczatkowe.Append($"{wagi[i][j][k]:F4} ");
+                    }
+                    sbPoczatkowe.AppendLine();
+                }
+            }
+            labelWagiPoczatkowe.Text = sbPoczatkowe.ToString();
+        }
+
+        public void GenerujWagiKoncowe(double[][][] wagi)
+        {
+            StringBuilder sbKoncowe = new StringBuilder("Wagi końcowe:\n");
+            for (int i = 0; i < wagi.Length; i++)
+            {
+                for (int j = 0; j < wagi[i].Length; j++)
+                {
+                    sbKoncowe.Append($"Warstwa {i}, Neuron {j}: ");
+                    for (int k = 0; k < wagi[i][j].Length; k++)
+                    {
+                        sbKoncowe.Append($"{wagi[i][j][k]:F4} ");
+                    }
+                    sbKoncowe.AppendLine();
+                }
+            }
+            labelWagiKoncowe.Text = sbKoncowe.ToString();
+        }
+        private void WszystkieWynikiPrzed(double[][] testoweWejscia, int[] struktura, double[][][] wagi)
+        {
+            var sb = new StringBuilder();
+
+            foreach (var wejscie in testoweWejscia)
+            {
+                var wyjscie = SiecNeuronowa(wejscie, wagi, struktura).Last();
+                string wejscieStr = string.Join(",  ", wejscie);
+                string wyjscieStr = string.Join(",  ", wyjscie.Select(x => x.ToString("0.###")));
+                sb.AppendLine($"[{wejscieStr}] -> [{wyjscieStr}]");
+            }
+
+            WYniki12.Text = sb.ToString();
+        }
+
+        private void WypiszWszystkieWyniki(double[][] testoweWejscia, int[] struktura, double[][][] wagi)
+        {
+            var sb = new StringBuilder();
+
+            foreach (var wejscie in testoweWejscia)
+            {
+                var wyjscie = SiecNeuronowa(wejscie, wagi, struktura).Last();
+                string wejscieStr = string.Join(",  ", wejscie);
+                string wyjscieStr = string.Join(",  ", wyjscie.Select(x => x.ToString("0.###")));
+                sb.AppendLine($"[{wejscieStr}] -> [{wyjscieStr}]");
+            }
+
+            WszystkieWyniki.Text = sb.ToString();
+        }
+
+
+    }
+}
